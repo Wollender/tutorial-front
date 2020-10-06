2907f214c64572dab8199c4926e452eb9a663c0d
孙瑶宁 <sunyaoning@odmi.cn>
2020-10-06 14:09:36 +0800

    modify config/index.js

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
-
+const PORT = process.env.PORT || 5000
 module.exports = {
   dev: {
 
@@ -24,7 +24,7 @@ module.exports = {
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     // host: '192.168.1.109', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: PORT, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
